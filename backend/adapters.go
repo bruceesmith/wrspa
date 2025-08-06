7f61445555c9e04b1c8f93e47ac3247ac955770a
bruceesmith <bruceesmith@gmail.com>
2025-08-06 16:21:40 +1000

    Add adapters and interfaces to facilitate testing

---
+++ backend/adapters.go
@@ -0,0 +1,68 @@
+package wrserver
+
+import (
+	"net/http"
+
+	"github.com/bruceesmith/terminator"
+)
+
+type clientAdapter struct {
+	client ClientInterface
+}
+
+func newClientAdapter() (c *clientAdapter) {
+	return &clientAdapter{client: &Client{}}
+}
+
+func (ca *clientAdapter) Get(path string) (body []byte, err error) {
+	return ca.client.Get(path)
+}
+
+func (ca *clientAdapter) GetRandom() (path string) {
+	return ca.client.GetRandom()
+}
+
+type serverAdapter struct {
+	server ServerInterface
+}
+
+func newServerAdapter(port, static string, client ClientInterface) (s *serverAdapter, err error) {
+	svr, err := NewServer(port, static, client)
+	return &serverAdapter{server: svr}, err
+}
+
+// func (sa *serverAdapter) MultiHandler(mux http.Handler) http.Handler {
+// 	return sa.server.MultiHandler(mux)
+// }
+
+func (sa *serverAdapter) API(w http.ResponseWriter, r *http.Request) {
+	sa.server.API(w, r)
+}
+
+func (sa *serverAdapter) MarshalFailure(function string, err error, response any) string {
+	return sa.server.MarshalFailure(function, err, response)
+}
+
+func (sa *serverAdapter) Serve(t *terminator.Terminator) {
+	sa.server.Serve(t)
+}
+
+func (sa *serverAdapter) Settings(w http.ResponseWriter, r *http.Request) {
+	sa.server.Settings(w, r)
+}
+
+func (sa *serverAdapter) SPAFile(w http.ResponseWriter, r *http.Request) {
+	sa.server.SPAFile(w, r)
+}
+
+func (sa *serverAdapter) SpecialRandom(w http.ResponseWriter, r *http.Request) {
+	sa.server.SpecialRandom(w, r)
+}
+
+func (sa *serverAdapter) WikiPage(w http.ResponseWriter, r *http.Request) {
+	sa.server.WikiPage(w, r)
+}
+
+func (sa *serverAdapter) WikipediaFile(w http.ResponseWriter, r *http.Request) {
+	sa.server.WikipediaFile(w, r)
+}
---
+++ backend/interfaces.go
@@ -0,0 +1,26 @@
+package wrserver
+
+import (
+	"net/http"
+
+	"github.com/bruceesmith/terminator"
+)
+
+// ClientInterface is an interface for the Client struct
+type ClientInterface interface {
+	Get(path string) (body []byte, err error)
+	GetRandom() (path string)
+}
+
+// ServerInterface is an interface for the Server struct
+type ServerInterface interface {
+	API(w http.ResponseWriter, r *http.Request)
+	MarshalFailure(function string, err error, response any) string
+	Serve(t *terminator.Terminator)
+	Settings(w http.ResponseWriter, r *http.Request)
+	SPAFile(w http.ResponseWriter, r *http.Request)
+	SpecialRandom(w http.ResponseWriter, r *http.Request)
+	WikiPage(w http.ResponseWriter, r *http.Request)
+	WikipediaFile(w http.ResponseWriter, r *http.Request)
+	// MultiHandler(mux http.Handler) http.Handler
+}
