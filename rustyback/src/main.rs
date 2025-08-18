ed564bbae93caedddf4cf10cb53c11ed59f6424a
bruceesmith <bruceesmith@gmail.com>
2025-08-18 16:16:02 +1000

    Add some Rust logging

--- rustyback/src/main.rs
+++ rustyback/src/main.rs
@@ -1,8 +1,10 @@
 use rustyback::run;
 use rustyback::server::ServerError;
+use env_logger;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     if let Err(e) = run().await {
         match e {
             ServerError::InvalidPort(p) => eprintln!("Invalid port: {}", p),
